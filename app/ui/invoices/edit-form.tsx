48ca0dcc705d6fda6d86669865014b89dd74b2c1
Helaman Rodrigues <u711602997@byui.edu>
2024-06-20 11:03:54 -0300

    fixing errors

--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -19,12 +19,10 @@ export default function EditInvoiceForm({
   invoice: InvoiceForm;
   customers: CustomerField[];
 }) {
-  const initialState = { message: null, errors: {} };
   const updateInvoiceWithId = updateInvoice.bind(null, invoice.id);
-  const [state, formAction] = useActionState(updateInvoiceWithId, initialState);
 
   return (
-    <form action={formAction}>
+    <form action={updateInvoiceWithId}>
       <input type="hidden" name="id" value={invoice.id} />
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
         {/* Customer Name */}
